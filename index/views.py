56383bf12208035b94ffca553e977afa3c2f2b31
Jasmeet Singh <mailtojasmeet@gmail.com>
2023-01-11 12:10:14 +0530

    login html improved, session life reset on home view visit added

--- index/views.py
+++ index/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
 
 
 @login_required(login_url='login')
 def index(request):
     '''this is the home'''
+    settings.SESSION_COOKIE_AGE = 600
     context = {}
     return render(request, 'index/index.html', context)
